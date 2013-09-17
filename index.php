a82926e085e289baa71cb863cbfe5364ee5c2735
Thiru Pattusamy <tpattusamy@fas.harvard.edu>
2013-09-16 22:24:40 -0400

    Missed index.php changes last time after adding css

--- index.php
+++ index.php
@@ -1,8 +1,71 @@
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-<Title> Autobiography </Title>
+   <meta charset="utf-8" />
+   <title>Autobiography</title>
+   <link rel="stylesheet" href="main.css">
 </head>
 <body>
-<h1> Welcome to my Page </h1>
+<header>
+    <hgroup>
+        <h1>Autobiography</h1>
+        <h2>Its all about me :)</h2>
+    </hgroup>
+</header>
+
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Me</a></li>
+    </ul>
+</nav>
+
+<article>
+    <header>
+        
+        <h1>
+        <a href="#" title="Link to this post" rel="bookmark">Thirukumaran Pattusamy</a>
+        </h1>
+    </header>
+    <section>
+    
+    <p>
+    <img class="thumbnail" src="Thiru_profile.jpg" height="100" width="100">
+    I'm Thirukumaran Pattusamy currently living in North Carolina and working as Senior Software Engineer. Having 12 years of experience in Software industry.  I am primarily working in Mainframe Technologies, would like to learn web stuff to keep me current in latest technologies, a prime reason to take CSCIE15</p>
+    
+    </section>
+    <section>
+        <header>
+            <h1>My Favorites</h1>
+        </header>
+        <p>
+        <ul>
+        	<li>My favorite team - Patriots</li>
+        	<li>My favorite sports - Football and Cricket</li>
+        	<li>My favorite dish - Biriyani</li>
+        	<li>My favorite Movie - Cast Away</li>
+        	<li>My favorite Place - LasVegas</li>
+        </ul>
+        </p>
+    </section>
+</article>
+<aside>
+      <h4>Technical skills</h4>
+      <ul>
+      		<li>COBOL</li>
+      		<li>CICS</li>
+      		<li>JCL</li>
+      		<li>DB2</li>
+      		<li>JAVA</li>
+      </ul>
+   </aside>
+   
+
+<footer>
+    <p>&copy; 2013 Thiruweb.biz. All rights reserved.</p>
+</footer>
+   
+   
+   
 </body>
 </html>
